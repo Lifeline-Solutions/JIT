1a540783463a12503e6b661443e7e74f9b9d134f
kwambiee <kwambokaj2.jk@gmail.com>
2023-03-30 17:39:44 +0300

    Style the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+  root "users#splash"
+  # devise_scope :user do
+  #   authenticated :user do
+  #     root "users#index", as: :authenticated_root
+  #   end
+  #   unauthenticated :user do
+  #     root "users#splash", as: :unauthenticated_root
+  #   end
+  # end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
   resources :users, only: %i[index show]
 end
